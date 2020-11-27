a4d89a08b2eecceef721ed9816b247be28bf4c23
RITHEESH R <71439671+RITHEESH123@users.noreply.github.com>
2020-11-27 16:40:48 +0530

    ASSETS COMMIT including Scripts
    In this I have attached the complete assets that I have used in this game also included all the C# Scripts that I have coded for this game.

---
+++ Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    public GameObject box_Prefab;
+
+    public void SpawnBox()
+    {
+        GameObject box_obj = Instantiate(box_Prefab);
+
+        Vector3 temp = transform.position;
+        temp.z = 0;
+        box_obj.transform.position = temp;
+    }
+}
